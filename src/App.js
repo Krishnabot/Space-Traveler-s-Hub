14747a3266b4e2ef07dc4ede55fcbb5f207ffafd
renercrows <ungredda95@gmail.com>
2022-08-30 22:19:15 -0400

    Fix Linters

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-/* eslint linebreak-style: ["error", "windows"] */
 import './App.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/* eslint linebreak-style: ["error", "windows"] */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
