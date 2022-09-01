173107c511cd4ac90eb62bbefaeb89b5f4816a10
Krishna <libraryismyparadise@gmail.com>
2022-09-01 22:57:47 +0545

    Change image tag to img

--- src/components/RocketCards.js
+++ src/components/RocketCards.js
@@ -4,13 +4,15 @@ import styles from './RocketCards.module.css';
 const RocketCards = () => (
   <div className={styles.cards}>
     <div className={styles.card1}>
-      <image href="#">Placeholder Image</image>
+      <img href="#" alt="Static" />
+      Placeholder Image
       <h2>Falcon1</h2>
       <p>Some dummy text as placeholder</p>
       <button type="submit">Reserve Rocket</button>
     </div>
     <div className={styles.card2}>
-      <image href="#">Placeholder Image2</image>
+      <img href="#" alt="Static" />
+      Placeholder Image
       <h2>Falcon9</h2>
       <p>Some dummy text as placeholder</p>
       <button type="submit">Cancel Reservation</button>
