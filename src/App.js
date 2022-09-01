33c07c92214046bba1c886421c05d81bc788d992
Krishna <libraryismyparadise@gmail.com>
2022-09-02 01:45:33 +0545

    Redirect to  Rockets default page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import MissionsContainer from './components/MissionsContainer';
 import RocketCards from './components/RocketCards';
@@ -12,6 +12,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/Rockets" element={<RocketCards />} />
+        <Route path="/" element={<Navigate replace to="/Rockets" />} />
         <Route path="/Missions" element={<MissionsContainer />} />
         <Route path="/MyProfile" element={<ProfileContainer />} />
       </Routes>
